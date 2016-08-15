d067bd264dff914d7c6ebe04056735e2fc11b9df
Del <del@babel.com.au>
2016-08-15 16:20:42 +0700

    Add sidebar menu

---
+++ resources/views/adminlayouts/menu_item.blade.php
@@ -0,0 +1,25 @@
+@if (is_array($item))
+    <li class="treeview">
+        <a href="#"><i class='fa fa-link'></i> <span>{{$key}}</span> <i class="fa fa-angle-left pull-right"></i></a>
+        <ul class="treeview-menu">
+            @foreach ($item as $k => $subitem)
+                <?php echo view("adminlayouts.menu_item", array(
+                        'item' => $subitem,
+                        'key' => $k,
+                        'settingsPrefix' => $settingsPrefix,
+                        'pagePrefix' => $pagePrefix
+                ))?>
+            @endforeach
+        </ul>
+    </li>
+@else
+    <li>
+        @if (strpos($key, $settingsPrefix) === 0)
+            <a href="{{route('admin_settings', array(substr($key, strlen($settingsPrefix))))}}"><i class='fa fa-link'></i> <span>{{$item}}</span></a>
+        @elseif (strpos($key, $pagePrefix) === 0)
+            <a href="{{route('admin_page', array(substr($key, strlen($pagePrefix))))}}"><i class='fa fa-link'></i> <span>{{$item}}</span></a>
+        @else
+            <a href="{{route('admin_index', array($key))}}"><i class='fa fa-link'></i> <span>{{$item}}</span></a>
+        @endif
+    </li>
+@endif
