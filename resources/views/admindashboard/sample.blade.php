6e7eb5e147f2088a47ba17f5feb8155d574450c1
Del <del@babel.com.au>
2016-08-15 14:35:38 +0700

    Fixes while testing published code

--- resources/views/admindashboard/sample.blade.php
+++ resources/views/admindashboard/sample.blade.php
@@ -2,7 +2,7 @@
     <div class="col-xs-12">
         <div class="box">
             <div class="box-header">
-                <h3 class="box-title">Gateways</h3>
+                <h3 class="box-title">Dashboard</h3>
             </div><!-- /.box-header -->
             <div class="box-body">
                 Dashboard goes here.
