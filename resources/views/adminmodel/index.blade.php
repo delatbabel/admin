b755141971d81110e7c96778417a5178dd38ce51
Del <del@babel.com.au>
2016-08-16 10:03:54 +0700

    Convert view to blade

--- resources/views/adminmodel/index.blade.php
+++ resources/views/adminmodel/index.blade.php
@@ -6,7 +6,7 @@
         <section class="content-header">
             <h1>
                 {{ Config::get('administrator.title') }}
-                <small>Model Index</small>
+                <small>{{ $config->getOption('title') }}</small>
             </h1>
             <ol class="breadcrumb">
                 <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
@@ -26,68 +26,68 @@
     </div><!-- /.content-wrapper -->
 
     <script type="text/javascript">
-        var site_url = "<?php echo url('/') ?>",
-                base_url = "<?php echo $baseUrl ?>/",
-                asset_url = "<?php echo $assetUrl ?>",
-                file_url = "<?php echo route('admin_display_file', array($config->getOption('name'))) ?>",
-                rows_per_page_url = "<?php echo route('admin_rows_per_page', array($config->getOption('name'))) ?>",
-                route = "<?php echo $route ?>",
-                csrf = "<?php echo csrf_token() ?>",
-                language = "<?php echo config('app.locale') ?>",
-                adminData = {
-                    primary_key: "<?php echo $primaryKey ?>",
-                    <?php if ($itemId !== null) {?>
-                    id: "<?php echo $itemId ?>",
-                    <?php } ?>
-                    rows: <?php echo json_encode($rows) ?>,
-                    rows_per_page: <?php echo $dataTable->getRowsPerPage() ?>,
-                    sortOptions: <?php echo json_encode($dataTable->getSort()) ?>,
-                    model_name: "<?php echo $config->getOption('name') ?>",
-                    model_title: "<?php echo $config->getOption('title') ?>",
-                    model_single: "<?php echo $config->getOption('single') ?>",
-                    expand_width: <?php echo $formWidth ?>,
-                    actions: <?php echo json_encode($actions) ?>,
-                    global_actions: <?php echo json_encode($globalActions) ?>,
-                    filters: <?php echo json_encode($filters) ?>,
-                    edit_fields: <?php echo json_encode($arrayFields) ?>,
-                    data_model: <?php echo json_encode($dataModel) ?>,
-                    column_model: <?php echo json_encode($columnModel) ?>,
-                    action_permissions: <?php echo json_encode($actionPermissions) ?>,
-                    languages: <?php echo json_encode(trans('administrator::knockout')) ?>
-                };
+        var site_url = "{!! url('/') !!}",
+            base_url = "{!! $baseUrl !!}/",
+            asset_url = "{!! $assetUrl !!}",
+            file_url = "{!! route('admin_display_file', array($config->getOption('name'))) !!}",
+            rows_per_page_url = "{!! route('admin_rows_per_page', array($config->getOption('name'))) !!}",
+            route = "{!! $route !!}",
+            csrf = "{!! csrf_token() !!}",
+            language = "{!! config('app.locale') !!}",
+            adminData = {
+                primary_key: "{!! $primaryKey !!}",
+                @if ($itemId !== null)
+                id: "{!! $itemId !!}",
+                @endif
+                rows: {!! json_encode($rows) !!},
+                rows_per_page: {!! $dataTable->getRowsPerPage() !!},
+                sortOptions: {!! json_encode($dataTable->getSort()) !!},
+                model_name: "{!! $config->getOption('name') !!}",
+                model_title: "{!! $config->getOption('title') !!}",
+                model_single: "{!! $config->getOption('single') !!}",
+                expand_width: {!! $formWidth !!},
+                actions: {!! json_encode($actions) !!},
+                global_actions: {!! json_encode($globalActions) !!},
+                filters: {!! json_encode($filters) !!},
+                edit_fields: {!! json_encode($arrayFields) !!},
+                data_model: {!! json_encode($dataModel) !!},
+                column_model: {!! json_encode($columnModel) !!},
+                action_permissions: {!! json_encode($actionPermissions) !!},
+                languages: {!! json_encode(trans('administrator::knockout')) !!}
+            };
     </script>
 
     <style type="text/css">
 
         div.item_edit form.edit_form select, div.item_edit form.edit_form input[type=hidden], div.item_edit form.edit_form .select2-container {
-            width: <?php echo $formWidth - 59 ?>px !important;
+            width: {{ $formWidth - 59 }}px !important;
         }
 
         div.item_edit form.edit_form .cke {
-            width: <?php echo $formWidth - 67 ?>px !important;
+            width: {{ $formWidth - 67 }}px !important;
         }
 
         div.item_edit form.edit_form div.markdown textarea {
-            width: <?php echo intval(($formWidth - 75) / 2) - 12 ?>px !important;
-            max-width: <?php echo intval(($formWidth - 75) / 2) - 12 ?>px !important;
+            width: {{ intval(($formWidth - 75) / 2) - 12 }}px !important;
+            max-width: {{ intval(($formWidth - 75) / 2) - 12 }}px !important;
         }
 
         div.item_edit form.edit_form div.markdown div.preview {
-            width: <?php echo intval(($formWidth - 75) / 2) ?>px !important;
+            width: {{ intval(($formWidth - 75) / 2) }}px !important;
         }
 
         div.item_edit form.edit_form input[type="text"], div.item_edit form.edit_form input[type="password"], div.item_edit form.edit_form textarea {
-            max-width: <?php echo $formWidth - 75 ?>px !important;
-            width: <?php echo $formWidth - 75 ?>px !important;
+            max-width: {{ $formWidth - 75 }}px !important;
+            width: {{ $formWidth - 75 }}px !important;
         }
 
         div.item_edit form.edit_form > div.image img, div.item_edit form.edit_form > div.image div.image_container {
-            max-width: <?php echo $formWidth - 65 ?>px;
+            max-width: {{ $formWidth - 65 }}px;
         }
 
     </style>
 
-    <input type="hidden" name="_token" value="<?php echo csrf_token()?>" />
+    <input type="hidden" name="_token" value="{{ csrf_token() }}" />
 
     <script id="adminTemplate" type="text/html">
     @include('adminmodel.table')
