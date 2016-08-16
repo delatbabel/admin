37b6d2c7cf947c2e4fb107b615b5aada1917a7a3
Del <del@babel.com.au>
2016-08-16 09:27:30 +0700

    Bug fix

--- resources/views/adminmodel/index.blade.php
+++ resources/views/adminmodel/index.blade.php
@@ -5,8 +5,8 @@
         <!-- Content Header (Page header) -->
         <section class="content-header">
             <h1>
-                {{ $objects->admin_dashboard_header }}
-                <small>{{ $objects->admin_dashboard_description }}</small>
+                {{ Config::get('administrator.title') }}
+                <small>Model Index</small>
             </h1>
             <ol class="breadcrumb">
                 <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
