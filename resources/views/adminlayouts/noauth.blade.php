638e89ede0dfe6bc0ab895aadb6c699e2b645839
Del <del@babel.com.au>
2016-08-17 11:11:13 +0700

    Fix view title

--- resources/views/adminlayouts/noauth.blade.php
+++ resources/views/adminlayouts/noauth.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>{{ config('administrator.title') or "Test Application" }}</title>
+    <title>{{ config('administrator.title') }}</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Bootstrap 3.3.4 -->
     <link href="/bower_components/admin-lte/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
