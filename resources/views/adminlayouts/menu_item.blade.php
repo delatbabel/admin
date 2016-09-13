d4675aa77a60d5bd1bd798c7b882612b8a069eaf
Del <del@babel.com.au>
2016-09-13 15:45:30 +0700

    Fixing menu layout

--- resources/views/adminlayouts/menu_item.blade.php
+++ resources/views/adminlayouts/menu_item.blade.php
@@ -1,6 +1,11 @@
 @if (is_array($item))
     <li class="treeview">
-        <a href="#"><i class='fa fa-link'></i> <span>{{$key}}</span> <i class="fa fa-angle-left pull-right"></i></a>
+        <a href="#"><i class='fa'></i>
+            <span>{{$key}}</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+        </a>
         <ul class="treeview-menu">
             @foreach ($item as $k => $subitem)
                 <?php echo view("adminlayouts.menu_item", array(
@@ -15,11 +20,11 @@
 @else
     <li>
         @if (strpos($key, $settingsPrefix) === 0)
-            <a href="{{route('admin_settings', array(substr($key, strlen($settingsPrefix))))}}"><i class='fa fa-link'></i> <span>{{$item}}</span></a>
+            <a href="{{route('admin_settings', array(substr($key, strlen($settingsPrefix))))}}"><i class='fa fa-link'></i> {{$item}}</a>
         @elseif (strpos($key, $pagePrefix) === 0)
-            <a href="{{route('admin_page', array(substr($key, strlen($pagePrefix))))}}"><i class='fa fa-link'></i> <span>{{$item}}</span></a>
+            <a href="{{route('admin_page', array(substr($key, strlen($pagePrefix))))}}"><i class='fa fa-link'></i> {{$item}}</a>
         @else
-            <a href="{{route('admin_index', array($key))}}"><i class='fa fa-link'></i> <span>{{$item}}</span></a>
+            <a href="{{route('admin_index', array($key))}}"><i class='fa fa-link'></i> {{$item}}</a>
         @endif
     </li>
 @endif
