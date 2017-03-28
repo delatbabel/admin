ee894dce0ca258aa7613942f44d29931ef3e9565
Dang Nguyen <haidangibm@gmail.com>
2017-03-28 14:09:58 +0700

    Fixes #712 Add default setter option for Html field type

--- src/Fields/Html.php
+++ src/Fields/Html.php
@@ -4,6 +4,15 @@ namespace DDPro\Admin\Fields;
 class Html extends Field
 {
     /**
+     * The specific defaults for subclasses to override
+     *
+     * @var array
+     */
+    protected $defaults = [
+        'setter' => true,
+    ];
+
+    /**
      * The specific rules for subclasses to override
      *
      * @var array
