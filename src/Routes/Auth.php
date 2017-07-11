1d13cdd42dcab8cfd4f8e9f359e6fe7b798a17bb
Del <del@babel.com.au>
2017-07-11 21:34:01 +0000

    Apply fixes from StyleCI

--- src/Routes/Auth.php
+++ src/Routes/Auth.php
@@ -6,63 +6,63 @@
 
 // Authentication
 Route::get('/login', [
-    'as' => 'auth.login.form',
+    'as'   => 'auth.login.form',
     'uses' => '\DDPro\Admin\Http\Controllers\Auth\SessionController@getLogin'
 ]);
 Route::post('/login', [
-    'as' => 'auth.login.attempt',
+    'as'   => 'auth.login.attempt',
     'uses' => '\DDPro\Admin\Http\Controllers\Auth\SessionController@postLogin'
 ]);
 Route::get('/logout', [
-    'as' => 'auth.logout',
+    'as'   => 'auth.logout',
     'uses' => '\DDPro\Admin\Http\Controllers\Auth\SessionController@getLogout'
 ]);
 
 // Dashboard
 Route::get('dashboard', [
-    'as' => 'dashboard',
+    'as'   => 'dashboard',
     'uses' => '\DDPro\Admin\Http\Controllers\DashboardController@index'
 ]);
 
 // Registration
 Route::get('register', [
-    'as' => 'auth.register.form',
+    'as'   => 'auth.register.form',
     'uses' => '\DDPro\Admin\Http\Controllers\Auth\RegistrationController@getRegister'
 ]);
 Route::post('register', [
-    'as' => 'auth.register.attempt',
+    'as'   => 'auth.register.attempt',
     'uses' => '\DDPro\Admin\Http\Controllers\Auth\RegistrationController@postRegister'
 ]);
 
 // Activation
 Route::get('activate/{code}', [
-    'as' => 'auth.activation.attempt',
+    'as'   => 'auth.activation.attempt',
     'uses' => '\DDPro\Admin\Http\Controllers\Auth\RegistrationController@getActivate'
 ]);
 Route::get('resend', [
-    'as' => 'auth.activation.request',
+    'as'   => 'auth.activation.request',
     'uses' => '\DDPro\Admin\Http\Controllers\Auth\RegistrationController@getResend'
 ]);
 Route::post('resend', [
-    'as' => 'auth.activation.resend',
+    'as'   => 'auth.activation.resend',
     'uses' => '\DDPro\Admin\Http\Controllers\Auth\RegistrationController@postResend'
 ]);
 
 // Password Reset
 Route::get('password/reset/{code}', [
-    'as' => 'auth.password.reset.form',
+    'as'   => 'auth.password.reset.form',
     'uses' => '\DDPro\Admin\Http\Controllers\Auth\PasswordController@getReset'
 ]);
 Route::post('password/reset/{code}', [
-    'as' => 'auth.password.reset.attempt',
+    'as'   => 'auth.password.reset.attempt',
     'uses' => '\DDPro\Admin\Http\Controllers\Auth\PasswordController@postReset'
 ]);
 Route::get('password/reset', [
-    'as' => 'auth.password.request.form',
+    'as'   => 'auth.password.request.form',
     'uses' => '\DDPro\Admin\Http\Controllers\Auth\PasswordController@getRequest'
 ]);
 Route::post('password/reset', [
-    'as' => 'auth.password.request.attempt',
+    'as'   => 'auth.password.request.attempt',
     'uses' => '\DDPro\Admin\Http\Controllers\Auth\PasswordController@postRequest'
 ]);
 
