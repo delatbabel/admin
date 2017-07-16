cefbd5ef471d0ca5799bed211d217641a554ad32
Del <del@babel.com.au>
2017-07-17 06:22:37 +0700

    View tidy

--- database/seeds/views/admin/dashboard/admin.blade.php
+++ database/seeds/views/admin/dashboard/admin.blade.php
@@ -2,32 +2,32 @@
 
 @section('content')
     <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
+        {{-- Content Header (Page header) --}}
         <section class="content-header">
             <h1>
                 {{ Config::get('administrator.title') }}
                 <small>Dashboard</small>
             </h1>
             <ol class="breadcrumb">
-                <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-                <li class="active">Here</li>
+                <li><a href="{{ route('admin_dashboard') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+                <li class="active">Dashboard</li>
             </ol>
         </section>
 
-        <!-- Main content -->
+        {{-- Main content --}}
         <section class="content">
             <div class='row'>
                 <div class="col-xs-12">
                     <div class="box">
                         <div class="box-header">
                             <h3 class="box-title">Dashboard</h3>
-                        </div><!-- /.box-header -->
+                        </div>{{-- /.box-header --}}
                         <div class="box-body">
                             Dashboard goes here.
-                        </div><!-- /.box-body -->
-                    </div><!-- /.box -->
-                </div><!-- /.col-xs-12 -->
-            </div><!-- /.row -->
-        </section><!-- /.content -->
-    </div><!-- /.content-wrapper -->
+                        </div>{{-- /.box-body --}}
+                    </div>{{-- /.box --}}
+                </div>{{-- /.col-xs-12 --}}
+            </div>{{-- /.row --}}
+        </section>{{-- /.content --}}
+    </div>{{-- /.content-wrapper --}}
 @endsection
