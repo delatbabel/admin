0b7dc1d20e8c252b9e615639f474972da7cdef57
Del <del@babel.com.au>
2017-08-02 11:39:30 +0700

    Fix PSR2

--- src/Http/ViewComposers/ModelViewComposer.php
+++ src/Http/ViewComposers/ModelViewComposer.php
@@ -1,12 +1,12 @@
 <?php
 namespace DDPro\Admin\Http\ViewComposers;
 
-use DDPro\Admin\Fields\Factory as FieldFactory;
-use DDPro\Admin\DataTable\Columns\Factory as ColumnFactory;
 use DDPro\Admin\Actions\Factory as ActionFactory;
+use DDPro\Admin\DataTable\Columns\Factory as ColumnFactory;
 use DDPro\Admin\DataTable\DataTable;
-use Log;
+use DDPro\Admin\Fields\Factory as FieldFactory;
 use Illuminate\View\View;
+use Log;
 
 /**
  * Class ModelViewComposer
